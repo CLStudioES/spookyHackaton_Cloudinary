e396ca61081113c4ca2b385364e1df5a8cade4a3
Carlos López Merino <karlosky12@gmail.com>
2024-10-10 13:26:39 +0200

    Empezamos con el index

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,8 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+		},
 	},
 	plugins: [
 		animations
