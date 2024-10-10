b8dbf8ae86a0be257b4c82554a03ea53033ff0b9
Carlos <karlosky12@gmail.com>
2024-10-11 00:14:37 +0200

    Seguimos con la primera página

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,18 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
+  routes: [{
+    path: '/index',
+    handler: () => import('./pages/index.astro')
+  }, {
+    path: '/personaje',
+    handler: () => import('./pages/personaje.astro')
+  }],
+  adapter: vercel()
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,17 @@ import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			/*Basic*/
+			'white': '#ffffff',
+			'black': '#000000',
+			/*Trickle*/
+			'trickle-black': '#030303',
+			'trickle-violet': '#9356A3',
+			'trickle-orange': '#E97714',
+			'trickle-green': '#ADEF13',
+			'trickle-purple': '#3D224B',
+		},
 		extend: {
 		},
 	},
